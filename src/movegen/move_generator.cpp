5d7f24e9473c479d1e42c509cbc6f94ead7584d0
devude7 <dawidmrowczynski7@gmail.com>
2026-07-19 17:38:21 +0200

    feat: add pieces move gen and tests

--- src/movegen/move_generator.cpp
+++ src/movegen/move_generator.cpp
@@ -10,6 +10,8 @@ void generate_bishop_moves(const Board& board, int from, std::vector<Move>& move
 void generate_rook_moves(const Board& board, int from, std::vector<Move>& moves);
 void generate_queen_moves(const Board& board, int from, std::vector<Move>& moves);
 void generate_king_moves(const Board& board, int from, std::vector<Move>& moves);
+void generate_sliding_moves(const Board& board, int from, const int directions[][2], int direction_count, std::vector<Move>& moves);
+void add_pawn_move(int from, int to, bool is_promotion, std::vector<Move>& moves);
 
 } 
 
@@ -44,9 +46,47 @@ std::vector<Move> generate_pseudo_legal_moves(const Board& board) {
 namespace {
 
 void generate_pawn_moves(const Board& board, int from, std::vector<Move>& moves) {
-    static_cast<void>(board);
-    static_cast<void>(from);
-    static_cast<void>(moves);
+    Color color = board.side_to_move;
+    int direction = color == Color::White ? 1 : -1;
+    int start_rank = color == Color::White ? 1 : 6;
+    int promotion_rank = color == Color::White ? 7 : 0;
+    int from_file = file_of(from);
+    int from_rank = rank_of(from);
+
+    int one_step_rank = from_rank + direction;
+    if (one_step_rank >= 0 && one_step_rank < 8) {
+        int one_step = one_step_rank * 8 + from_file;
+
+        if (is_empty_square(board, one_step)) {
+            add_pawn_move(from, one_step, one_step_rank == promotion_rank, moves);
+
+            int two_step_rank = from_rank + 2 * direction;
+            if (from_rank == start_rank && two_step_rank >= 0 && two_step_rank < 8) {
+                int two_step = two_step_rank * 8 + from_file;
+
+                if (is_empty_square(board, two_step)) {
+                    moves.push_back(Move{from, two_step, MoveType::Normal, PieceType::None});
+                }
+            }
+        }
+    }
+
+    for (int file_delta : {-1, 1}) {
+        int to_file = from_file + file_delta;
+        int to_rank = from_rank + direction;
+
+        if (to_file < 0 || to_file >= 8 || to_rank < 0 || to_rank >= 8) {
+            continue;
+        }
+
+        int to = to_rank * 8 + to_file;
+
+        if (is_occupied_by(board, to, opposite(color))) {
+            add_pawn_move(from, to, to_rank == promotion_rank, moves);
+        } else if (to == board.en_passant_square) {
+            moves.push_back(Move{from, to, MoveType::EnPassant, PieceType::None});
+        }
+    }
 }
 
 void generate_knight_moves(const Board& board, int from, std::vector<Move>& moves) {
@@ -83,27 +123,111 @@ void generate_knight_moves(const Board& board, int from, std::vector<Move>& move
 }
 
 void generate_bishop_moves(const Board& board, int from, std::vector<Move>& moves) {
-    static_cast<void>(board);
-    static_cast<void>(from);
-    static_cast<void>(moves);
+    constexpr int directions[4][2] = {
+        { 1,  1},
+        { 1, -1},
+        {-1, -1},
+        {-1,  1}
+    };
+    generate_sliding_moves(board, from, directions, 4, moves);
 }
 
 void generate_rook_moves(const Board& board, int from, std::vector<Move>& moves) {
-    static_cast<void>(board);
-    static_cast<void>(from);
-    static_cast<void>(moves);
+    constexpr int directions[4][2] = {
+        { 1,  0},
+        { 0, -1},
+        {-1,  0},
+        { 0,  1}
+    };
+    generate_sliding_moves(board, from, directions, 4, moves);
 }
 
 void generate_queen_moves(const Board& board, int from, std::vector<Move>& moves) {
-    static_cast<void>(board);
-    static_cast<void>(from);
-    static_cast<void>(moves);
+    constexpr int directions[8][2] = {
+        { 1,  0},
+        { 1,  1},
+        { 0,  1},
+        {-1,  1},
+        {-1,  0},
+        {-1, -1},
+        { 0, -1},
+        { 1, -1}
+    };
+    generate_sliding_moves(board, from, directions, 8, moves);
 }
 
 void generate_king_moves(const Board& board, int from, std::vector<Move>& moves) {
-    static_cast<void>(board);
-    static_cast<void>(from);
-    static_cast<void>(moves);
+    constexpr int king_offsets[8][2] = {
+        { 1,  0},
+        { 1,  1},
+        { 0,  1},
+        {-1,  1},
+        {-1,  0},
+        {-1, -1},
+        { 0, -1},
+        { 1, -1}
+    };
+
+    int from_file = file_of(from);
+    int from_rank = rank_of(from);
+
+    for (const auto& offset : king_offsets) {
+        int to_file = from_file + offset[0];
+        int to_rank = from_rank + offset[1];
+
+        if (to_file < 0 || to_file >= 8 || to_rank < 0 || to_rank >= 8) {
+            continue;
+        }
+
+        int to = to_rank * 8 + to_file;
+
+        if (is_occupied_by(board, to, board.side_to_move)) {
+            continue;
+        }
+
+        moves.push_back(Move{from, to, MoveType::Normal, PieceType::None});
+    }
+}
+
+void generate_sliding_moves(const Board& board, int from, const int directions[][2], int direction_count, std::vector<Move>& moves) {
+    int from_file = file_of(from);
+    int from_rank = rank_of(from);
+
+    for (int direction_index = 0; direction_index < direction_count; ++direction_index) {
+        int file_delta = directions[direction_index][0];
+        int rank_delta = directions[direction_index][1];
+        int to_file = from_file + file_delta;
+        int to_rank = from_rank + rank_delta;
+
+        while (to_file >= 0 && to_file < 8 && to_rank >= 0 && to_rank < 8) {
+            int to = to_rank * 8 + to_file;
+
+            if (is_occupied_by(board, to, board.side_to_move)) {
+                break;
+            }
+
+            moves.push_back(Move{from, to, MoveType::Normal, PieceType::None});
+
+            if (!is_empty_square(board, to)) {
+                break;
+            }
+
+            to_file += file_delta;
+            to_rank += rank_delta;
+        }
+    }
+}
+
+void add_pawn_move(int from, int to, bool is_promotion, std::vector<Move>& moves) {
+    if (!is_promotion) {
+        moves.push_back(Move{from, to, MoveType::Normal, PieceType::None});
+        return;
+    }
+
+    moves.push_back(Move{from, to, MoveType::Promotion, PieceType::Queen});
+    moves.push_back(Move{from, to, MoveType::Promotion, PieceType::Rook});
+    moves.push_back(Move{from, to, MoveType::Promotion, PieceType::Bishop});
+    moves.push_back(Move{from, to, MoveType::Promotion, PieceType::Knight});
 }
 
 } 
---
+++ tests/unit/movegen_tests.cpp
@@ -0,0 +1,216 @@
+#include <cassert>
+#include <vector>
+
+#include "chess/board/fen.hpp"
+#include "chess/core/move.hpp"
+#include "chess/movegen/move_generator.hpp"
+
+namespace {
+
+bool contains_move(const std::vector<chess::Move>& moves, int from, int to) {
+    for (const chess::Move& move : moves) {
+        if (move.from == from && move.to == to) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool contains_move(
+    const std::vector<chess::Move>& moves,
+    int from,
+    int to,
+    chess::MoveType type,
+    chess::PieceType promotion) {
+    for (const chess::Move& move : moves) {
+        if (move.from == from && move.to == to && move.type == type && move.promotion == promotion) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void test_knight_in_center() {
+    chess::Board board = chess::board_from_fen("8/8/8/8/3N4/8/8/8 w - - 0 1");
+
+    std::vector<chess::Move> moves = chess::generate_pseudo_legal_moves(board);
+
+    assert(moves.size() == 8);
+    assert(contains_move(moves, 27, 10));
+    assert(contains_move(moves, 27, 12));
+    assert(contains_move(moves, 27, 17));
+    assert(contains_move(moves, 27, 21));
+    assert(contains_move(moves, 27, 33));
+    assert(contains_move(moves, 27, 37));
+    assert(contains_move(moves, 27, 42));
+    assert(contains_move(moves, 27, 44));
+}
+
+void test_knight_in_corner() {
+    chess::Board board = chess::board_from_fen("8/8/8/8/8/8/8/N7 w - - 0 1");
+
+    std::vector<chess::Move> moves = chess::generate_pseudo_legal_moves(board);
+
+    assert(moves.size() == 2);
+    assert(contains_move(moves, 0, 10));
+    assert(contains_move(moves, 0, 17));
+}
+
+void test_king_in_center() {
+    chess::Board board = chess::board_from_fen("8/8/8/8/3K4/8/8/8 w - - 0 1");
+
+    std::vector<chess::Move> moves = chess::generate_pseudo_legal_moves(board);
+
+    assert(moves.size() == 8);
+    assert(contains_move(moves, 27, 18));
+    assert(contains_move(moves, 27, 19));
+    assert(contains_move(moves, 27, 20));
+    assert(contains_move(moves, 27, 26));
+    assert(contains_move(moves, 27, 28));
+    assert(contains_move(moves, 27, 34));
+    assert(contains_move(moves, 27, 35));
+    assert(contains_move(moves, 27, 36));
+}
+
+void test_king_in_corner() {
+    chess::Board board = chess::board_from_fen("8/8/8/8/8/8/8/K7 w - - 0 1");
+
+    std::vector<chess::Move> moves = chess::generate_pseudo_legal_moves(board);
+
+    assert(moves.size() == 3);
+    assert(contains_move(moves, 0, 1));
+    assert(contains_move(moves, 0, 8));
+    assert(contains_move(moves, 0, 9));
+}
+
+void test_bishop_in_center() {
+    chess::Board board = chess::board_from_fen("8/8/8/8/3B4/8/8/8 w - - 0 1");
+
+    std::vector<chess::Move> moves = chess::generate_pseudo_legal_moves(board);
+
+    assert(moves.size() == 13);
+    assert(contains_move(moves, 27, 0));
+    assert(contains_move(moves, 27, 6));
+    assert(contains_move(moves, 27, 48));
+    assert(contains_move(moves, 27, 63));
+}
+
+void test_rook_in_center() {
+    chess::Board board = chess::board_from_fen("8/8/8/8/3R4/8/8/8 w - - 0 1");
+
+    std::vector<chess::Move> moves = chess::generate_pseudo_legal_moves(board);
+
+    assert(moves.size() == 14);
+    assert(contains_move(moves, 27, 3));
+    assert(contains_move(moves, 27, 24));
+    assert(contains_move(moves, 27, 31));
+    assert(contains_move(moves, 27, 59));
+}
+
+void test_queen_in_center() {
+    chess::Board board = chess::board_from_fen("8/8/8/8/3Q4/8/8/8 w - - 0 1");
+
+    std::vector<chess::Move> moves = chess::generate_pseudo_legal_moves(board);
+
+    assert(moves.size() == 27);
+    assert(contains_move(moves, 27, 0));
+    assert(contains_move(moves, 27, 3));
+    assert(contains_move(moves, 27, 31));
+    assert(contains_move(moves, 27, 63));
+}
+
+void test_sliding_piece_stops_before_own_piece() {
+    chess::Board board = chess::board_from_fen("8/8/3P4/8/3R4/8/8/8 w - - 0 1");
+
+    std::vector<chess::Move> moves = chess::generate_pseudo_legal_moves(board);
+
+    assert(contains_move(moves, 27, 35));
+    assert(!contains_move(moves, 27, 43));
+    assert(!contains_move(moves, 27, 51));
+}
+
+void test_sliding_piece_captures_enemy_and_stops() {
+    chess::Board board = chess::board_from_fen("8/8/3p4/8/3R4/8/8/8 w - - 0 1");
+
+    std::vector<chess::Move> moves = chess::generate_pseudo_legal_moves(board);
+
+    assert(contains_move(moves, 27, 35));
+    assert(contains_move(moves, 27, 43));
+    assert(!contains_move(moves, 27, 51));
+}
+
+void test_white_pawn_initial_double_move() {
+    chess::Board board = chess::board_from_fen("8/8/8/8/8/8/4P3/8 w - - 0 1");
+
+    std::vector<chess::Move> moves = chess::generate_pseudo_legal_moves(board);
+
+    assert(moves.size() == 2);
+    assert(contains_move(moves, 12, 20));
+    assert(contains_move(moves, 12, 28));
+}
+
+void test_black_pawn_initial_double_move() {
+    chess::Board board = chess::board_from_fen("8/4p3/8/8/8/8/8/8 b - - 0 1");
+
+    std::vector<chess::Move> moves = chess::generate_pseudo_legal_moves(board);
+
+    assert(moves.size() == 2);
+    assert(contains_move(moves, 52, 44));
+    assert(contains_move(moves, 52, 36));
+}
+
+void test_pawn_captures() {
+    chess::Board board = chess::board_from_fen("8/8/8/3p1p2/4P3/8/8/8 w - - 0 1");
+
+    std::vector<chess::Move> moves = chess::generate_pseudo_legal_moves(board);
+
+    assert(moves.size() == 3);
+    assert(contains_move(moves, 28, 35));
+    assert(contains_move(moves, 28, 36));
+    assert(contains_move(moves, 28, 37));
+}
+
+void test_pawn_promotion() {
+    chess::Board board = chess::board_from_fen("8/P7/8/8/8/8/8/8 w - - 0 1");
+
+    std::vector<chess::Move> moves = chess::generate_pseudo_legal_moves(board);
+
+    assert(moves.size() == 4);
+    assert(contains_move(moves, 48, 56, chess::MoveType::Promotion, chess::PieceType::Queen));
+    assert(contains_move(moves, 48, 56, chess::MoveType::Promotion, chess::PieceType::Rook));
+    assert(contains_move(moves, 48, 56, chess::MoveType::Promotion, chess::PieceType::Bishop));
+    assert(contains_move(moves, 48, 56, chess::MoveType::Promotion, chess::PieceType::Knight));
+}
+
+void test_en_passant() {
+    chess::Board board = chess::board_from_fen("8/8/8/3pP3/8/8/8/8 w - d6 0 1");
+
+    std::vector<chess::Move> moves = chess::generate_pseudo_legal_moves(board);
+
+    assert(moves.size() == 2);
+    assert(contains_move(moves, 36, 44));
+    assert(contains_move(moves, 36, 43, chess::MoveType::EnPassant, chess::PieceType::None));
+}
+
+} // namespace
+
+int main() {
+    test_knight_in_center();
+    test_knight_in_corner();
+    test_king_in_center();
+    test_king_in_corner();
+    test_bishop_in_center();
+    test_rook_in_center();
+    test_queen_in_center();
+    test_sliding_piece_stops_before_own_piece();
+    test_sliding_piece_captures_enemy_and_stops();
+    test_white_pawn_initial_double_move();
+    test_black_pawn_initial_double_move();
+    test_pawn_captures();
+    test_pawn_promotion();
+    test_en_passant();
+
+    return 0;
+}
